e16a43b25da34a82997c88d82f5543264b4bd55b
Juned <juned.developer786@gmail.com>
2023-08-04 21:43:30 +0530

    Product API added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
 import Counter from './Components/Counter';
+import Products from './Components/Products';
 
 function App(props) {
   return(
     <div className='head-div'>
-    <h1 className='first-h1'>Counter App</h1>
-    <Counter title="Value"/>
+    <h1 className='first-h1'>My Shopping Cart</h1>
+    <Products />
   </div>
   
   );
---
+++ src/Components/Products.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+
+export default function Products(){
+    
+    const [products,setProducts]= useState([]);
+    const [isLoading,setIsLoading] = useState(true);
+
+    const fetchProducts = async ()=>{
+        const res = await fetch('https://fakestoreapi.com/products');
+        const data = await res.json();
+        setProducts(data);
+        setIsLoading(false);
+    }
+
+    useEffect(()=>{
+        fetchProducts();
+    },[products])
+   
+    return(
+        <div>
+           <h2>Product Details</h2>
+           
+           {isLoading?"Loading....":null}
+           <div className="product-head">
+                {
+                    products.map((product,index)=>(
+                        <div className="products" key={index}>
+                            {product.title}
+                            <img loading="lazy" className="product-image" alt="product-image" src={product.image}></img>
+                             Price: {product.price} USD
+                        </div>
+                    ))
+                }
+            </div>   
+        </div>
+
+    )
+}
+
