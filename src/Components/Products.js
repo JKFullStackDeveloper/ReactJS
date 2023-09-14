47be1b5a5e038efc0865727c73262adda69c4a5a
Juned <juned.developer786@gmail.com>
2023-09-14 12:16:02 +0530

    search feature added

--- src/Components/Products.js
+++ src/Components/Products.js
@@ -1,10 +1,20 @@
+import { useState } from "react";
 import { useProductContext } from "../Context/ProductContext";
 import ProductCard from "./ProductCard";
 
 export default function Products() {
 
-    const {data,isLoading,loadError} = useProductContext();
-     
+    const { data, isLoading, loadError } = useProductContext();
+    const [serachItem, setSearchItem] = useState("");
+
+    let filterData= serachItem===""?data:
+    data.filter((product)=>{
+        let lowerProduct = product.title.toLowerCase();
+        let lowerSearch = serachItem.toLowerCase();
+        return lowerProduct.includes(lowerSearch);
+    })
+
+
     if (isLoading) {
         return (
             <div><i class=" spinner fa-solid fa-spinner"><span>Loading...</span></i></div>
@@ -14,11 +24,14 @@ export default function Products() {
     } else if (data.length > 0) {
         return (
             <div>
-                <h2>Product Details</h2>
+                <div className='serach-bar'>
+                    <input onChange={(e)=>{setSearchItem(e.target.value)}} value={serachItem} className='search-txt' type='search' placeholder='search products' />
+                    <button className='search-btn'><i class="fa-solid fa-magnifying-glass"></i></button>
+                </div>
                 <div className="product-head">
                     {
-                        data.map((product, index) => (
-                            <ProductCard key={index} product={product}/>
+                        filterData.map((product, index) => (
+                            <ProductCard key={index} product={product} />
                         ))
                     }
                 </div>
